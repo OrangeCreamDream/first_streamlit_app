5c2d53bacf88d98c72ef0c3752543e35e90a4c91
OrangeCreamDream <136368634+OrangeCreamDream@users.noreply.github.com>
2023-06-12 13:59:20 -0600

    Update streamlit_app.py
    Define fruits_selected and fruits_to_show variables. Display fruits_to_show in streamlit.dataframe(fruits_to_show)

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,9 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include
 # I believe my previous error occured because I put the set_index assignment after my multi-select function
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 
 
 
