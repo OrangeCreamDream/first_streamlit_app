fbbe35d4237f5e75d3091082bbae20af96aa27e4
OrangeCreamDream <136368634+OrangeCreamDream@users.noreply.github.com>
2023-06-12 13:52:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,9 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # make multi-select values human-readable
 my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado'])
+# Let's put a pick list here so they can pick the fruit they want to include
+# I believe my previous error occured because I put the set_index assignment after my multi-select function
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 streamlit.dataframe(my_fruit_list)
 
 
